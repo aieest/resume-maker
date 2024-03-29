4532c1944016ed593b45c528b247cbc1efd85a40
aieest <aie.estidola@gmail.com>
2024-03-29 13:13:38 +1300

    Imported ResumeEditor to app.jsx

--- resume-builder-front/src/App.jsx
+++ resume-builder-front/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import ResumeEditor from './resume-editor/resume-editor'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <ResumeEditor />
     </>
   )
 }
