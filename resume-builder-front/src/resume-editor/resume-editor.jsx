c37f8b0b575dfd04ff52aa963b713d8e6b67d755
aieest <aie.estidola@gmail.com>
2024-03-30 21:39:09 +1300

    Created suffix form and preview

--- resume-builder-front/src/resume-editor/resume-editor.jsx
+++ resume-builder-front/src/resume-editor/resume-editor.jsx
@@ -4,6 +4,7 @@ function ResumeEditor() {
     const [familyName, setFamilyName] = useState("familyName");
     const [middleName, setMiddleName] = useState("middleName");
     const [givenName, setGivenName] = useState("givenName");
+    const [suffixName, setSuffixName] = useState("suffixName");
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -13,12 +14,14 @@ function ResumeEditor() {
             setMiddleName(value);
         } else if (name === "givenName") {
             setGivenName(value);
+        } else if (name === "suffixName") {
+            setSuffixName(value);
         }
     };
     return (
         <main className="resumer-editor">
             <EditorForm handleChange={handleChange} />
-            <ResumePreview familyName={familyName} middleName={middleName} givenName={givenName} />
+            <ResumePreview familyName={familyName} middleName={middleName} givenName={givenName} suffixName={suffixName}/>
         </main>
     );
 }
@@ -26,19 +29,21 @@ function ResumeEditor() {
 function EditorForm({ handleChange }) {
     return (
     <section className="editor-form">
-        <input type="text" name="familyName" className="last form-name" onChange={handleChange} />
-        <input type="text" name="givenName" className="given form-name" onChange={handleChange} />
-        <input type="text" name="middleName" className="middle form-name" onChange={handleChange} />
+        <input type="text" name="familyName" className="family form-name" onChange={handleChange} placeholder="Last Name"/>
+        <input type="text" name="givenName" className="given form-name" onChange={handleChange} placeholder="Given Name"/>
+        <input type="text" name="middleName" className="middle form-name" onChange={handleChange} placeholder="Middle Name"/>
+        <input type="text" name="suffixName" className="suffix form-name" onChange={handleChange} placeholder="Suffix"/>
     </section>
     )
 }
 
-function ResumePreview({ familyName, middleName, givenName }) {
+function ResumePreview({ familyName, middleName, givenName, suffixName }) {
     return (
         <section className="resume-preview">
             <div className="preview name">
                 <p>{familyName}, </p>
                 <p>{givenName} </p>
+                <p>{suffixName} </p>
                 <p>{middleName}</p>
             </div>
         </section>
