fe7523a78aa7932953679c6dbb86a7370690d811
aieest <aie.estidola@gmail.com>
2024-04-03 11:02:39 +1300

    Created contact form and preveiw component

---
+++ resume-builder-front/src/resume-editor/resume-editor-form/contacts.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ContactForm({ handleContactsChange }) {
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        handleContactsChange(name, value);
+    };
+
+    return (
+        <section className="editor-form">
+            <input type="text" name="mobileContact" className="mobile form-contact" onChange={handleInputChange} placeholder="Mobile Number"/>
+            <input type="text" name="homeContact" className="home form-contact" onChange={handleInputChange} placeholder="Home Landline"/>
+            <input type="text" name="emailContact" className="email form-contact" onChange={handleInputChange} placeholder="Email Address"/>
+        </section>
+    );
+}
+
+export default ContactForm;
